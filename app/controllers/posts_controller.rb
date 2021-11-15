f86ceec93fe70a00ed0b5b49b18d3a17027c2a11
joshraiborde <joshraiborde@gmail.com>
2021-11-15 13:07:33 -0500

    added new create show methods, added private method that included params require for post image

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostController < ApplicationController
+
+    def new
+
+    end
+
+
+    def create
+
+    end
+
+    
+    def show
+
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:image)
+    end
+
+
+
+end
+
